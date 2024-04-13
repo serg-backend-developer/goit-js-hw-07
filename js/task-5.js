33cfc52bc41eeca8d5ab0d9d818892828edf509c
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-04-13 17:25:46 +0200

    remove tasks description

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,3 @@
-//*Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль
-//*по кліку на button.change-color і задає це значення кольору текстовим вмістом
-//*для span.color.
-//*Для генерування випадкового кольору використовуй функцію getRandomHexColor().
-
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215)
 		.toString(16)
